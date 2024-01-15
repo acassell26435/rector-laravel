d7362fc589dd89ff65151931d83a2125a2538f69
acassell26435 <nanicassell@gmail.com>
2024-01-15 03:16:55 -0500

    3

--- src/Laravel/resources/views/include/breadcum.blade.php
+++
@@ -1,5 +0,0 @@
-</a><h3 class="admin-page-inner-heading">{{ $title }}</h3>
-<ol class="breadcrumb">
-  <li><a href="{{ url('/admin') }}"><i class="fa fa-dashboard"></i>{{ $from }}</a></li>
-  <li class="active">{{ $to }}</li>
-</ol>
