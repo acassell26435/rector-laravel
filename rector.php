2a8fbfaf17b133af2870c1381d5047893ba713b0
acassell26435 <nanicassell@gmail.com>
2024-01-19 10:24:07 -0500

    r

--- rector.php
+++ rector.php
@@ -10,14 +10,14 @@ use Rector\Set\ValueObject\SetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
 
-    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+    $rectorConfig->paths([__DIR__ . '/src/auto', __DIR__ . '/tests']);
 
     $rectorConfig->skip([
         // for tests
         '*/Source/*',
         '*/Fixture/*',
         __DIR__ . '/tests',
-        __DIR__ . '/src/gpauto/vendor',
+        __DIR__ . '/src/auto/vendor',
 
         // skip for handle scoped, like in the rector-src as well
         // @see https://github.com/rectorphp/rector-src/blob/7f73cf017214257c170d34db3af7283eaeeab657/rector.php#L71
