899f82d50aff1216ff319d16b4006b6a7138c3b8
GitHub Action <actions@github.com>
2024-01-19 09:33:07 +0000

    [ci-review] Rector Rectify

--- src/gpauto/app/Http/Controllers/PWAController.php
+++ src/gpauto/app/Http/Controllers/PWAController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Artisan;
 use DotenvEditor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Image;
 
 /*==========================================
