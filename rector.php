7ca2520c29beeb1c197713d5da7511ab1efec212
acassell26435 <nanicassell@gmail.com>
2024-01-19 10:44:32 -0500

    yy

--- rector.php
+++ rector.php
@@ -17,9 +17,6 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/tests',
         __DIR__ . '/src/gpauto',
 
-        // skip for handle scoped, like in the rector-src as well
-        // @see https://github.com/rectorphp/rector-src/blob/7f73cf017214257c170d34db3af7283eaeeab657/rector.php#L71
-        StringClassNameToClassConstantRector::class,
     ]);
     $rectorConfig->disableParallel();
 
