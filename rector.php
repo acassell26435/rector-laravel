9eb917c3d57e0ca3ca1843a54cfa637625458a65
acassell26435 <nanicassell@gmail.com>
2024-01-19 05:45:50 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -1,20 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
-use RectorLaravel\Set\LaravelSetList;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/src/gpauto',
-    ]);
+    $rectorConfig->importNames();
+
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
 
     $rectorConfig->skip([
+        // for tests
+        '*/Source/*',
+        '*/Fixture/*',
         __DIR__ . '/tests',
-        __DIR__ . '/src/gpauto/vendor',
+        __DIR__ . '/src/gpauto/vendor',                   
 
+        // skip for handle scoped, like in the rector-src as well
+        // @see https://github.com/rectorphp/rector-src/blob/7f73cf017214257c170d34db3af7283eaeeab657/rector.php#L71
+        StringClassNameToClassConstantRector::class,
     ]);
     $rectorConfig->disableParallel();
-    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82,LaravelSetList::LARAVEL_100, SetList::DEAD_CODE, SetList::CODE_QUALITY, SetList::NAMING]);
+
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82, SetList::DEAD_CODE, SetList::CODE_QUALITY, SetList::NAMING]);
 };
+   
