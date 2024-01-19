8e5c7e461384c5ae683e6b5e668ef9fc526fa965
acassell26435 <nanicassell@gmail.com>
2024-01-19 10:43:10 -0500

    hh

--- rector.php
+++ rector.php
@@ -8,14 +8,14 @@ use Rector\Config\RectorConfig;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
 
-    $rectorConfig->paths([__DIR__ . '/src/auto']);
+    $rectorConfig->paths([__DIR__ . '/src']);
 
     $rectorConfig->skip([
         // for tests
         '*/Source/*',
         '*/Fixture/*',
         __DIR__ . '/tests',
-        __DIR__ . '/src/auto/vendor',
+        __DIR__ . '/src/gpauto',
 
         // skip for handle scoped, like in the rector-src as well
         // @see https://github.com/rectorphp/rector-src/blob/7f73cf017214257c170d34db3af7283eaeeab657/rector.php#L71
