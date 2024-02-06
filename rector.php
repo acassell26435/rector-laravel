5588ee40ffc0dc45d2b8c8e0481040455b1ee5b5
acassell26435 <nanicassell@gmail.com>
2024-02-06 02:21:14 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -18,5 +18,5 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
     $rectorConfig->disableParallel();
 
-    $rectorConfig->sets([LaravelSetList::LARAVEL_80]);
+    $rectorConfig->sets([LaravelSetList::LARAVEL_90]);
 };
